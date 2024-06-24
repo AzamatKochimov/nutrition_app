fb8f0a3fbe3f17baa535854efb48f1082535d77b
muminovabdulloh <muminovabdullokh@gmail.com>
2024-06-24 10:56:28 +0500

    Abdulloh Muminov, Colors have added.

--- lib/src/core/style/colors.dart
+++ lib/src/core/style/colors.dart
@@ -8,9 +8,42 @@ sealed class AppColors {
   static const Color transparent = Color(0x00000000);
   static const Color white = Color(0xffffffff);
   static const Color black = Color(0xFF000000);
+
   //every colors
-
-
+  static const Color c1C3418 = Color(0xff1C3418);
+  static const Color c6CB663 = Color(0xff6CB663);
+  static const Color c91C788 = Color(0xff91C788);
+  static const Color cCFE7CB = Color(0xffCFE7CB);
+  static const Color c330600 = Color(0xff330600);
+  static const Color c4D0A00 = Color(0xff4D0A00);
+  static const Color cFF806E = Color(0xffFF806E);
+  static const Color cFF8473 = Color(0xffFF8473);
+  static const Color cFF9385 = Color(0xffFF9385);
+  static const Color cFFC0B8 = Color(0xffFFC0B8);
+  static const Color cFFD485 = Color(0xffFFD485);
+  static const Color c4D3200 = Color(0xff4D3200);
+  static const Color c9E9BC7 = Color(0xff9E9BC7);
+  static const Color cA3A0CA = Color(0xffA3A0CA);
+  static const Color cC6C4DE = Color(0xffC6C4DE);
+  static const Color c0D0D0D = Color(0xff0D0D0D);
+  static const Color c272727 = Color(0xff272727);
+  static const Color c2E2E2E = Color(0xff2E2E2E);
+  static const Color c696969 = Color(0xff696969);
+  static const Color c666666 = Color(0xff666666);
+  static const Color c707070 = Color(0xff707070);
+  static const Color c767676 = Color(0xff767676);
+  static const Color c777777 = Color(0xff777777);
+  static const Color c7B7B7B = Color(0xff7B7B7B);
+  static const Color c909090 = Color(0xff909090);
+  static const Color c999999 = Color(0xff999999);
+  static const Color cA1A1A1 = Color(0xffA1A1A1);
+  static const Color cA9A9A9 = Color(0xffA9A9A9);
+  static const Color cFFF2F0 = Color(0xffFFF2F0);
+  static const Color cFFF8EE = Color(0xffFFF8EE);
+  static const Color cF4F4F4 = Color(0xffF4F4F4);
+  static const Color cF5F5F5 = Color(0xffF5F5F5);
+  static const Color cFFF8EB = Color(0xffFFF8EB);
+  static const Color cEFF7EE = Color(0xffEFF7EE);
 
   // gradients
   static const List<Color> homeMainScrollItems = <Color>[white, black];

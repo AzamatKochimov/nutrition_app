1b23aaaa120b540102fddcffd6ad2ac78c65a4e8
AzamatKochimov <kochimovazamat180@gmail.com>
2024-07-05 15:46:13 +0500

    Azamat 3-commit, provider is added to project

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,32 @@
 import "package:flutter/material.dart";
 import "package:flutter_screenutil/flutter_screenutil.dart";
+import "package:provider/provider.dart";
+import "package:provider_go_router_flutter_localizations_inherited_widget/src/feature/auth/controller/auth_controller.dart";
+import "package:provider_go_router_flutter_localizations_inherited_widget/src/feature/main/presentation/pages/home/controller/home_controller.dart";
 import "core/widget/app_material_context.dart";
 
 class App extends StatelessWidget {
   const App({super.key});
 
-  static void run() => runApp(const App());
+  static void run() => runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthController(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => HomeController(),
+        ),
+      ],
+      child: const App(),
+    ),
+  );
 
   @override
   Widget build(BuildContext context) => ScreenUtilInit(
-        designSize: const Size(360, 800),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (BuildContext context, Widget? child) => AppMaterialContext(),
-      );
+    designSize: const Size(450, 932),
+    minTextAdapt: true,
+    splitScreenMode: true,
+    builder: (BuildContext context, Widget? child) => AppMaterialContext(),
+  );
 }
